31e113cc3181677c67179d9b3490eefa41ea0eb1
Staffan Living <staffan.living@gmail.com>
2011-03-03 21:10:18 +0100

    Added settings for GenomeAnalysisTK and path to dbSNP

--- settings.js
+++ settings.js
@@ -3,7 +3,10 @@ exports.settings = {
 	samtools: "samtools",
 	node: "node",
 	bwa: "bwa",
+	gatk: "java -Xms512m -Xmx2048m -jar /usr/local/lib/GenomeAnalysisTK-1.0.5315/GenomeAnalysisTK.jar -et NO_ET",
+	dbSNP: "/ExamWork/dbSNP/dbsnp_130_hg18.rod",
 	resultsPath: "./tmp/results/",
 	modifiedGenomesPath: "./tmp/genomes/",
-	referenceGenomePath: "./tmp/genomes/reference/BRCAChromosomes.fa"
-}
+	referenceGenomePath: "./tmp/genomes/reference/BRCAChromosomes.fa",
+	removeIntermediateFiles: true
+};
