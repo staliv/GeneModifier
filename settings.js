1e84f3c15dbd4718050fd1aabc46e8b2898f006d
Staffan Living <staffan.living@gmail.com>
2011-03-12 13:39:29 +0100

    Added log level of GATK to settings

--- settings.js
+++ settings.js
@@ -5,6 +5,7 @@ exports.settings = {
 	bwa: "bwa",
 	gatk: "java -Xms512m -Xmx2048m -jar /usr/local/lib/GenomeAnalysisTK-1.0.5315/GenomeAnalysisTK.jar -et NO_ET",
 	gatkInterval: "chr17:38449838-38531026;chr13:31787617-31871809",
+	gatkLogLevel: "INFO", //INFO, ERROR, FATAL
 	downSampleToCoverage: 40000,
 	dbSNP: "/ExamWork/dbSNP/dbsnp_130_hg18.rod",
 	resultsPath: "./tmp/results/",
