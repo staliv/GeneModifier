bc1e088dc57339c20629c6766f03f98d547d3438
Staffan Living <staffan.living@gmail.com>
2011-03-13 12:17:14 +0100

    Extended range for gatk interval

--- settings.js
+++ settings.js
@@ -3,8 +3,10 @@ exports.settings = {
 	samtools: "samtools",
 	node: "node",
 	bwa: "bwa",
+	novoalign: "novoalign",
+	novoindex: "novoindex",
 	gatk: "java -Xms512m -Xmx2048m -jar /usr/local/lib/GenomeAnalysisTK-1.0.5315/GenomeAnalysisTK.jar -et NO_ET",
-	gatkInterval: "chr17:38449838-38531026;chr13:31787617-31871809",
+	gatkInterval: "chr13:31785617-31873809;chr17:38447838-38533026", //	gatkInterval: "chr13:31787617-31871809;chr17:38449838-38531026", 
 	gatkLogLevel: "INFO", //INFO, ERROR, FATAL
 	downSampleToCoverage: 40000,
 	dbSNP: "/ExamWork/dbSNP/dbsnp_130_hg18.rod",
